2bda088adc495eea0fc014ca675f4b7cd3bdbbc1
Simon JAILLET <sjaillet@gmail.com>
2015-11-17 03:41:32 +0100

    Creates the 1.0.7 release.

--- chai-jasmine.js
+++ chai-jasmine.js
@@ -3326,7 +3326,12 @@ module.exports = function (chai, _) {
     , flag = _.flag;
 
   // Overrides Spies so it won't rely on Jasmine's env
-  var spyRegistry = new jasmine.SpyRegistry();
+  var spies = [];
+  var spyRegistry = new jasmine.SpyRegistry({
+    currentSpies: function() {
+      return spies;
+    }
+  });
   jasmine.getEnv().spyOn = function() {
     return spyRegistry.spyOn.apply(spyRegistry, arguments);
   };
